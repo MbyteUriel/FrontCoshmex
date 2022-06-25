011f052bdd11d2fc00d585e61e6de40dd66252a3
urielmbyte <uriel@mbyte.com>
2022-06-25 13:43:33 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Hero from "./components/Hero.vue";
+import Categories from "./components/Categories.vue";
+import ProductsList from './components/ProductsList.vue'
+import StoreNavigation from "./components/StoreNavigation.vue";
+</script>
+
+<template>
+  <StoreNavigation />
+  <Hero />
+  <Categories />
+  <ProductsList />
+</template>
+
+<style>
+
+</style>
